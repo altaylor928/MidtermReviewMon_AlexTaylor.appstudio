dd949f55e6b5f3c3b4ef8956a3c3702cd5048e89
altaylor928 <60445248+altaylor928@users.noreply.github.com>
2020-03-02 14:51:50 -0600

    Initial commit

---
+++ forms/one/one.js
@@ -0,0 +1,10 @@
+/*
+Do the same program as #1 but using 
+controls and events (no prompts, alerts, etc.).
+*/
+
+/*
+let number1 = Number(prompt("Enter a number: "))
+let number2 = Number(prompt("Enter another number: "))
+alert(`The sum of the numbers is ${number1 + number2}.`)
+*/
---
+++ forms/two/two.js
@@ -0,0 +1,12 @@
+/*
+Write code (No controls) that gets two numbers from the user 
+and outputs the sum - use alerts, prompts, etc.
+*/
+
+/*
+btnSubmit.onclick=function(){
+      let number1 = Number(inptNum1.value)
+      let number2 = Number(inptNum2.value)
+      lblMessage.value = `The sum of the numbers is ${number1 + number2}.`
+}
+*/
